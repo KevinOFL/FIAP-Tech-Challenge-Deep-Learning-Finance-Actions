f51664c85072be7f4150b4af3599a84d1634fcd0
KevinOFL <kevinsilvaofl@gmail.com>
2025-12-13 14:06:57 -0300

    att modules train.py and modelo.py

--- src/api_deep_finances/dl/modelo.py
+++ src/api_deep_finances/dl/modelo.py
@@ -1,20 +1,33 @@
 import torch.nn as nn
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, num_layers=1):
         super(LSTMModel, self).__init__()
         
         # Definindo a arquitetura LSTM
         # input_size: número de features de entrada
         # hidden_size: quantidade de neurônios na camada oculta
         # batch_first=True para que a entrada seja (batch, seq, feature)
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True, num_layers=num_layers)
         
-        # Camada linear para mapear a saída da LSTM para o valor de previsão
-        self.linear = nn.Linear(in_features=hidden_size, out_features=1)
+        # Camada de dropout para evitar overfitting, desligando 20% dos neurônios durante o treinamento
+        #self.dropout = nn.Dropout(p=0.2)
+        
+        # Camada linear intermediária
+        self.linear1 = nn.Linear(in_features=hidden_size, out_features=32)
+        
+        # Adiciona não linearidade
+        self.relu = nn.ReLU()
+        
+        # Camada linear final para saída
+        self.linear_final = nn.Linear(in_features=32, out_features=1)
         
     def forward(self, x):
-        out, _ = self.lstm(x)           # Saída da LSTM e estado oculto
-        last_step = out[:, -1, :]       # Pegando a saída do último passo de tempo
-        return self.linear(last_step)   # Mapeando para a previsão final
+        out, _ = self.lstm(x)                   # Saída da LSTM e estado oculto
+        last_step = out[:, -1, :]               # Pegando a saída do último passo de tempo
+        #x = self.dropout(last_step)            # Aplicando dropout
+        x = self.linear1(last_step)             # Camada linear intermediária
+        x = self.relu(x)                        # Função de ativação ReLU
+        previsao = self.linear_final(x)         # Camada linear final
+        return previsao                         # previsão final
         
--- src/api_deep_finances/dl/train.py
+++ src/api_deep_finances/dl/train.py
@@ -1,10 +1,12 @@
 import torch.optim as optim
 import mlflow
 import mlflow.pytorch
+import pandas as pd
 import numpy as np
 import torch
 import sys
 import os
+from mlflow.models.signature import infer_signature
 
 # Usar esse bloco somente se tiver problemas de importação como eu tive !!!!
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,60 +21,140 @@ from modelo import LSTMModel
 from configs.log_config import logger
 
 PARAMS = {
-    'experimento': 'Deep_Finances_LSTM',
-    'input_size': 5,            # Número de features de entrada
-    'hidden_size': 100,         # Número de neurônios na camada oculta
-    'num_epochs': 500,          # Número de épocas para treinamento
-    'learning_rate': 0.002,     # Taxa de aprendizado
-    'window_size': 5,           # Tamanho da janela deslizante
-    'feature_column': 'Close'   # Coluna alvo para previsão
+    'experimento': 'Deep_Finances_LSTM_for_energy_actions',
+    'input_size': 5,                            # Número de features de entrada
+    'hidden_size': 64,                          # Número de neurônios na camada oculta
+    'num_layers': 2,                            # Número de camadas LSTM
+    'num_epochs': 1000,                         # Número de épocas para treinamento
+    'learning_rate': 0.001,                     # Taxa de aprendizado
+    'window_size': 20,                          # Tamanho da janela deslizante
+    'feature_column': 'Close',                  # Coluna alvo para previsão
+    'patience': 250,                            # Paciência para early stopping
+    'min_delta': 0.00001                        # Mudança mínima para considerar melhoria
 }
 
-logger.info("Carregando dados para treinamento...")
 
-X_train, y_train = load_data(path='./docs/planilhas_completas/data_actions_energy_1year.csv', collumn=PARAMS['feature_column'])
-y_train = y_train.view(-1, 1)  # Ajusta o shape de y_train para (batch_size, 1)
 
-logger.info(f"Dados carregados: X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+tickers = pd.read_csv('./docs/planilhas_completas/data_actions_energy_3year.csv')['Ticker'].unique().tolist()
 
-mlflow.set_experiment(PARAMS['experimento'])
+for i, ticker in enumerate(tickers):
+    logger.info(f"Treinando modelo para o ticker {ticker} ({i+1}/{len(tickers)})")
+    
+    logger.info(f"Carregando dados para treinamento...")
+    X_train, y_train, X_test, y_test = load_data(path='./docs/planilhas_completas/data_actions_energy_3year.csv', collumn=PARAMS['feature_column'], ticker=ticker, window_size=PARAMS['window_size'])
+    y_train = y_train.view(-1, 1)   # Ajusta o shape de y_train para (batch_size, 1)
+    y_test = y_test.view(-1, 1)     # Ajusta o shape de y_test para (batch_size, 1)
 
-logger.info("Inicializando treinamento do modelo com mlflow...")
-with mlflow.start_run():
-    mlflow.log_params(PARAMS)
+    logger.info(f"Dados carregados: X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+
+    mlflow.set_experiment(PARAMS['experimento'])
+
+    logger.info("Inicializando treinamento do modelo com mlflow...")
+    with mlflow.start_run():
+        # Metadados do experimento
+        mlflow.set_tag("ticker", ticker)
+        mlflow.set_tag("setor", "Energia")
+        mlflow.set_tag("model_type", "LSTM")
+        mlflow.set_tag("optimizer", "Adam")
+        mlflow.set_tag("developer", "Kevin")
+        mlflow.log_params(PARAMS)
     
-    modelo = LSTMModel(input_size=PARAMS['input_size'], hidden_size=PARAMS['hidden_size'])
+        modelo = LSTMModel(input_size=PARAMS['input_size'], hidden_size=PARAMS['hidden_size'], num_layers=PARAMS['num_layers'])
     
-    # Move modelo e dados para GPU se disponível
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    modelo.to(device)
-    X_train = X_train.to(device)
-    y_train = y_train.to(device)
+        # Move modelo e dados para GPU se disponível
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        modelo.to(device)
+        X_train = X_train.to(device)
+        y_train = y_train.to(device)
+        X_test = X_test.to(device)
+        y_test = y_test.to(device)
     
-    criterion = torch.nn.MSELoss()
-    optimizer = optim.Adam(modelo.parameters(), lr=PARAMS['learning_rate'])
-    
-    logger.info("Iniciando loop de treinamento...")
-    for epoch in range(PARAMS['num_epochs']):
-        modelo.train()
+        criterion = torch.nn.MSELoss()
+        optimizer = optim.Adam(modelo.parameters(), lr=PARAMS['learning_rate'])
         
-        outputs = modelo(X_train)           # Treinamento do modelo
-        loss = criterion(outputs, y_train)  # Cálculo da loss e validação do modelo
-        optimizer.zero_grad()               # Zerando os gradientes          
-        loss.backward()                     # Backpropagation  
-        optimizer.step()                    # Atualização dos pesos
-        
-        mlflow.log_metric("loss", loss.item(), step=epoch)
-        
-        if (epoch+1) % 10 == 0:
-            logger.info(f'Epoca [{epoch+1}/{PARAMS["num_epochs"]}], Loss: {loss.item():.5f}')
+        # Learning rate scheduler para reduzir a taxa de aprendizado se a loss não melhorar por 10 épocas
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10)
+
+        best_loss_mse = float('inf')
+        best_loss_rmse = float('inf')
+        patience_counter = 0
+
+        logger.info("Iniciando loop de treinamento...")
+        for epoch in range(PARAMS['num_epochs']):
+            modelo.train()
             
-    logger.info("Treinamento concluído. Salvando o modelo...")
-    input_sample = X_train[0:1].numpy()
-    mlflow.pytorch.log_model(modelo, "lstm_model", input_example=input_sample)
-    
-    logger.info("Modelo salvo com sucesso no MLflow.")
-    
-    logger.info("Salvando scalers usados no pré-processamento...")
-    mlflow.log_artifact("./src/api_deep_finances/dl/scalers/scaler_target.joblib")
-    mlflow.log_artifact("./src/api_deep_finances/dl/scalers/scaler_all.joblib")
+            outputs = modelo(X_train)                           # Treinamento do modelo
+            loss = criterion(outputs, y_train)                  # Cálculo da loss
+            rmse = torch.sqrt(loss)                             # Cálculo do RMSE   
+            mae = torch.mean(torch.abs(outputs - y_train))      # Cálculo do MAE
+            optimizer.zero_grad()                               # Zerando os gradientes          
+            loss.backward()                                     # Backpropagation  
+            optimizer.step()                                    # Atualização dos pesos
+            
+            
+            # Avaliação no conjunto de teste
+            modelo.eval()
+            with torch.no_grad():
+                test_outputs = modelo(X_test)
+                test_loss = criterion(test_outputs, y_test)
+                current_loss_mse = test_loss.item() 
+                test_rmse = torch.sqrt(test_loss)
+                current_loss_rmse = test_rmse.item()
+                test_mae = torch.mean(torch.abs(test_outputs - y_test))
+                
+            scheduler.step(test_loss)                           # Atualiza o scheduler com a loss de teste atual
+            
+            mlflow.log_metric("Train Loss MSE", loss.item(), step=epoch)
+            mlflow.log_metric("Test Loss MSE", test_loss.item(), step=epoch)
+            mlflow.log_metric("Train RMSE", rmse.item(), step=epoch)
+            mlflow.log_metric("Test RMSE", test_rmse.item(), step=epoch)
+            mlflow.log_metric("Train MAE", mae.item(), step=epoch)
+            mlflow.log_metric("Test MAE", test_mae.item(), step=epoch)
+
+            # Coloca o modelo de volta em modo de treinamento
+            modelo.train()
+            
+            # Early Stopping check
+            if current_loss_mse < (best_loss_mse - PARAMS['min_delta']):
+                best_loss_mse = current_loss_mse
+                best_loss_rmse = current_loss_rmse
+                patience_counter = 0
+            else:
+                patience_counter += 1
+                
+            if patience_counter >= PARAMS['patience']:
+                logger.info("----- Activate Early stopping -----")
+                logger.info(f'Early stopping at epoch {epoch+1} and loss not upgraded for {PARAMS["patience"]} epochs. The best loss RMSE was {best_loss_rmse:.5f}.')
+                break
+            
+            if (epoch+1) % 100 == 0:
+                logger.info(f'Epoca [{epoch+1}/{PARAMS["num_epochs"]}], Train Loss RMSE: {rmse.item():.5f} - Test Loss RMSE: {test_rmse.item():.5f}')
+        
+        
+        logger.info("Treinamento concluido. Salvando o modelo...")
+        
+        # Inferindo a assinatura do modelo para registro no MLflow
+        input_sample_numpy = X_train[0:1].cpu().numpy()
+        output_sample_numpy = modelo(X_train[0:1]).detach().cpu().numpy()
+        signature = infer_signature(input_sample_numpy, output_sample_numpy)
+        
+        input_sample = X_train[0:1].numpy()
+        
+        ticker = ticker.replace('.SA','')
+        
+        # Salvando o modelo treinado no MLflow
+        mlflow.pytorch.log_model(
+            modelo,
+            artifact_path='model',
+            input_example=input_sample,
+            registered_model_name=f"LSTMModel_{ticker}",
+            signature=signature
+        )
+
+        logger.info("Modelo salvo com sucesso no MLflow.")
+        
+        logger.info("Salvando scalers usados no pre-processamento...")
+        mlflow.log_artifact(f"./src/api_deep_finances/dl/scalers/scaler_target_{ticker}.joblib")
+        mlflow.log_artifact(f"./src/api_deep_finances/dl/scalers/scaler_all_{ticker}.joblib")
+        
+logger.info("Treinamento de todos os modelos concluído.")
