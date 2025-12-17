ce3b849fafa8e43e6aace43b8f02f459ae4073b5
KevinOFL <kevinsilvaofl@gmail.com>
2025-12-17 11:40:56 -0300

    att module configs logger

--- configs/log_config.py
+++ configs/log_config.py
@@ -2,17 +2,17 @@ import logging
 import sys
 import os
 
-def configure_logger():
+def configure_logger(aplication_name: str) -> logging.Logger:
     """Configures and returns a logger for the finance application."""
     
-    logger = logging.getLogger("finance_logger")
+    logger = logging.getLogger(aplication_name)
     logger.setLevel(logging.DEBUG)
     
     if not logger.handlers:
         formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(name)s | %(module)s | %(message)s') # Formato do log
         
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Pega a raiz do projeto
-        log_path = os.path.join(base_dir, 'logs', 'finance_app.log')
+        log_path = os.path.join(base_dir, 'logs', f'{aplication_name}.log')
         
         # Garante que a pasta logs existe
         os.makedirs(os.path.dirname(log_path), exist_ok=True)
@@ -29,4 +29,6 @@ def configure_logger():
         
     return logger
 
-logger = configure_logger()
+logger_api = configure_logger("api")
+logger_db = configure_logger("database")
+logger_training = configure_logger("training")
